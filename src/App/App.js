52cc045e58d0c0172f40613a18bb6142db77f213
sanasarwat <sanasarwat@hotmail.com>
2021-06-03 08:15:43 -0700

    intial commit

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import AppAdmin from "./AppAdmin";
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <AppAdmin />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Employees/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import EmployeesList from "./EmployeesList";
+
+export default function Employees() {
+  return (
+    <div>
+      <EmployeesList />
+    </div>
+  );
+}
